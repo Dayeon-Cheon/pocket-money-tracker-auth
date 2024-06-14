830a9d4e9b708c32a5b29193aff8e960ecf1050e
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-06-15 00:11:33 +0900

    feat: display user info in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import { useQuery } from "@tanstack/react-query";
+import { getUserInfo } from "../api/auth";
 import styled from "styled-components";
 
 const Header = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const {
+    data: userInfo = [],
+    isLoading,
+    error,
+  } = useQuery({ queryKey: ["user"], queryFn: getUserInfo });
 
   const handleLogout = () => {
     const confirmLogout = window.confirm("정말로 로그아웃 하시겠습니까?");
@@ -14,6 +22,14 @@ const Header = () => {
       navigate("/");
     }
   };
+
+  if (isLoading) {
+    <div>로딩 중입니다.</div>;
+  }
+
+  if (error) {
+    <div>에러가 발생했습니다. 다시 시도해 주세요.</div>;
+  }
 
   return (
     <HeaderContainer>
@@ -24,6 +40,10 @@ const Header = () => {
       <StyledNav>
         {isAuthenticated ? (
           <>
+            <div>
+              <img src={userInfo.avatar} alt="프로필 이미지" />
+            </div>
+            <div>{userInfo.nickname}님 안녕하세요.</div>
             <button onClick={handleLogout}>로그아웃</button>
           </>
         ) : (
